a7ee3c6724baeda076a9cd73fb70868706cb3597
awood314 <awood314@gmail.com>
2024-06-09 17:44:29 -0700

    LPF1 implementation

--- plugin/src/PluginProcessor.cpp
+++ plugin/src/PluginProcessor.cpp
@@ -1,154 +1,140 @@
 #include <juce_core/juce_core.h>
 
-#include "generated/lib.rs.h"
-
 #include "PluginEditor.h"
 #include "PluginProcessor.h"
+#include "lib.rs.h"
 
-namespace wprust
-{
+namespace wprust {
 
 WPRustProcessor::WPRustProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-: AudioProcessor(BusesProperties()
+    : AudioProcessor(
+          BusesProperties()
 #if !JucePlugin_IsMidiEffect
 #if !JucePlugin_IsSynth
-                     .withInput("Input", juce::AudioChannelSet::stereo(), true)
+              .withInput("Input", juce::AudioChannelSet::stereo(), true)
 #endif
-                     .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+              .withOutput("Output", juce::AudioChannelSet::stereo(), true)
 #endif
-                     )
+      )
 #endif
-{}
-
-WPRustProcessor::~WPRustProcessor()
-{}
-
-const juce::String WPRustProcessor::getName() const
 {
-    return JucePlugin_Name;
 }
 
-bool WPRustProcessor::acceptsMidi() const
-{
+WPRustProcessor::~WPRustProcessor() {}
+
+const juce::String WPRustProcessor::getName() const { return JucePlugin_Name; }
+
+bool WPRustProcessor::acceptsMidi() const {
 #if JucePlugin_WantsMidiInput
-    return true;
+  return true;
 #else
-    return false;
+  return false;
 #endif
 }
 
-bool WPRustProcessor::producesMidi() const
-{
+bool WPRustProcessor::producesMidi() const {
 #if JucePlugin_ProducesMidiOutput
-    return true;
+  return true;
 #else
-    return false;
+  return false;
 #endif
 }
 
-bool WPRustProcessor::isMidiEffect() const
-{
+bool WPRustProcessor::isMidiEffect() const {
 #if JucePlugin_IsMidiEffect
-    return true;
+  return true;
 #else
-    return false;
+  return false;
 #endif
 }
 
-double WPRustProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
+double WPRustProcessor::getTailLengthSeconds() const { return 0.0; }
+
+int WPRustProcessor::getNumPrograms() {
+  return 1; // NB: some hosts don't cope very well if you tell them there are 0
+            // programs, so this should be at least 1, even if you're not really
+            // implementing programs.
 }
 
-int WPRustProcessor::getNumPrograms()
-{
-    return 1; // NB: some hosts don't cope very well if you tell them there are 0 programs,
-              // so this should be at least 1, even if you're not really implementing programs.
+int WPRustProcessor::getCurrentProgram() { return 0; }
+
+void WPRustProcessor::setCurrentProgram(int) {}
+
+const juce::String WPRustProcessor::getProgramName(int) { return {}; }
+
+void WPRustProcessor::changeProgramName(int, const juce::String &) {}
+
+void WPRustProcessor::prepareToPlay(double sampleRate, int) {
+  rust::reset(_filters[0], sampleRate);
+  rust::reset(_filters[1], sampleRate);
 }
 
-int WPRustProcessor::getCurrentProgram()
-{
-    return 0;
-}
-
-void WPRustProcessor::setCurrentProgram(int)
-{}
-
-const juce::String WPRustProcessor::getProgramName(int)
-{
-    return {};
-}
-
-void WPRustProcessor::changeProgramName(int, const juce::String&)
-{}
-
-void WPRustProcessor::prepareToPlay(double, int)
-{
-}
-
-void WPRustProcessor::releaseResources()
-{
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
+void WPRustProcessor::releaseResources() {
+  // When playback stops, you can use this as an opportunity to free up any
+  // spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool WPRustProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
-{
+bool WPRustProcessor::isBusesLayoutSupported(const BusesLayout &layouts) const {
 #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused(layouts);
-    return true;
+  juce::ignoreUnused(layouts);
+  return true;
 #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-        && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
+  // This is the place where you check if the layout is supported.
+  // In this template code we only support mono or stereo.
+  // Some plugin hosts, such as certain GarageBand versions, will only
+  // load plugins that support stereo bus layouts.
+  if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() &&
+      layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+    return false;
 
-        // This checks if the input layout matches the output layout
+    // This checks if the input layout matches the output layout
 #if !JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
+  if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+    return false;
 #endif
 
-    return true;
+  return true;
 #endif
 }
 #endif
 
-void WPRustProcessor::processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&)
-{
-    test(8);
+void WPRustProcessor::processBlock(juce::AudioBuffer<float> &audioBuffer,
+                                   juce::MidiBuffer &) {
+  // set parameters
+  rust::set_parameters(_filters[0], {.frequency = _frequencyParam.get()});
+  rust::set_parameters(_filters[1], {.frequency = _frequencyParam.get()});
+
+  // process
+  for (int i = 0; i < audioBuffer.getNumChannels(); i++) {
+    if (i < 2) {
+      const auto &channelRead = audioBuffer.getReadPointer(i);
+      const auto &channelWrite = audioBuffer.getWritePointer(i);
+      for (int j = 0; j < audioBuffer.getNumSamples(); j++) {
+        channelWrite[j] = rust::process(_filters[i], channelRead[j]);
+      }
+    }
+  }
 }
 
 //==============================================================================
-bool WPRustProcessor::hasEditor() const
-{
-    return true; // (change this to false if you choose to not supply an editor)
+bool WPRustProcessor::hasEditor() const {
+  return true; // (change this to false if you choose to not supply an editor)
 }
 
-juce::AudioProcessorEditor* WPRustProcessor::createEditor()
-{
-    return new WPRustProcessorEditor(*this);
+juce::AudioProcessorEditor *WPRustProcessor::createEditor() {
+  return new WPRustProcessorEditor(*this);
 }
 
 //==============================================================================
-void WPRustProcessor::getStateInformation(juce::MemoryBlock&)
-{
-}
+void WPRustProcessor::getStateInformation(juce::MemoryBlock &) {}
 
-void WPRustProcessor::setStateInformation(const void*, int)
-{
-}
+void WPRustProcessor::setStateInformation(const void *, int) {}
 
 } // namespace wprust
 
-
 // This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{
-    return new wprust::WPRustProcessor();
+juce::AudioProcessor *JUCE_CALLTYPE createPluginFilter() {
+  return new wprust::WPRustProcessor();
 }
--- plugin/src/PluginProcessor.h
+++ plugin/src/PluginProcessor.h
@@ -1,47 +1,53 @@
 #pragma once
 
+#pragma GCC diagnostic ignored "-Wdollar-in-identifier-extension"
+#include "lib.rs.h"
+
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <juce_audio_processors/juce_audio_processors.h>
 
-namespace wprust
-{
+namespace wprust {
 
-class WPRustProcessor : public juce::AudioProcessor
-{
+class WPRustProcessor : public juce::AudioProcessor {
 public:
-    WPRustProcessor();
-    ~WPRustProcessor() override;
+  WPRustProcessor();
+  ~WPRustProcessor() override;
 
-    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
-    void releaseResources() override;
+  void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+  void releaseResources() override;
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-    bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
+  bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
 #endif
 
-    void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+  void processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
 
-    juce::AudioProcessorEditor* createEditor() override;
-    bool hasEditor() const override;
+  juce::AudioProcessorEditor *createEditor() override;
+  bool hasEditor() const override;
 
-    const juce::String getName() const override;
+  const juce::String getName() const override;
 
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
+  bool acceptsMidi() const override;
+  bool producesMidi() const override;
+  bool isMidiEffect() const override;
+  double getTailLengthSeconds() const override;
 
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram(int index) override;
-    const juce::String getProgramName(int index) override;
-    void changeProgramName(int index, const juce::String& newName) override;
+  int getNumPrograms() override;
+  int getCurrentProgram() override;
+  void setCurrentProgram(int index) override;
+  const juce::String getProgramName(int index) override;
+  void changeProgramName(int index, const juce::String &newName) override;
 
-    void getStateInformation(juce::MemoryBlock& destData) override;
-    void setStateInformation(const void* data, int sizeInBytes) override;
+  void getStateInformation(juce::MemoryBlock &destData) override;
+  void setStateInformation(const void *data, int sizeInBytes) override;
 
 private:
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(WPRustProcessor)
+  juce::AudioParameterFloat _frequencyParam{
+      "frequency", "Frequency", {20.f, 20480.f}, 1000.f};
+
+  rust::AudioFilter _filters[2];
+
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(WPRustProcessor)
 };
 
-} // namespace splice
+} // namespace wprust
