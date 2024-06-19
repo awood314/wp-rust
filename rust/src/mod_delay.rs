ecf72a69c8b41918375562c58591c9386305756c
awood314 <awood314@gmail.com>
2024-06-19 15:27:29 -0700

    lfo

--- rust/src/mod_delay.rs
+++ rust/src/mod_delay.rs
@@ -1,5 +1,14 @@
+use std::f64::consts::PI;
+
 #[cxx::bridge(namespace = "wprust::rust::mod")]
 mod ffi {
+    struct LFO {
+        frequency: f64,
+        counter: f64,
+        phase_inc: f64,
+        sample_rate: f64,
+    }
+
     struct ModulatedDelayParameters {
         rate: f64,
         depth: f64,
@@ -8,8 +17,8 @@ mod ffi {
 
     struct ModulatedDelay {
         params: ModulatedDelayParameters,
+        lfo: LFO,
         // delay
-        // lfo
     }
 
     extern "Rust" {
@@ -18,6 +27,38 @@ mod ffi {
         fn set_parameters(filter: &mut ModulatedDelay, params: ModulatedDelayParameters);
 
         fn process(filter: &mut ModulatedDelay, xn: f64) -> f64;
+    }
+}
+
+fn parabolic_sin(x: f64) -> f64 {
+    const B: f64 = 4.0 / PI;
+    const C: f64 = -4.0 / (PI * PI);
+    const P: f64 = 0.225;
+
+    let y = B * x + C * x * x.abs();
+    P * (y * y.abs() - y) + y
+}
+
+impl ffi::LFO {
+    fn reset(&mut self, sample_rate: f64) {
+        self.sample_rate = sample_rate;
+        self.phase_inc = self.frequency / sample_rate;
+        self.counter = 0.0;
+    }
+
+    fn render(&mut self) -> f64 {
+        // Sin wave
+        let angle = self.counter * 2.0 * PI - PI;
+
+        // increment counter and wrap
+        self.counter += self.phase_inc;
+        self.counter = match self.counter {
+            counter if counter >= 1.0 => counter - 1.0,
+            counter if counter <= 0.0 => counter + 1.0,
+            _ => self.counter,
+        };
+
+        parabolic_sin(-angle)
     }
 }
 
